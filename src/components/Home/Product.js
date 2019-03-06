62c3160b1249059499b5d7e761b3efcb023279c0
Param Saluja <salujaparam@gmail.com>
2019-03-06 19:46:31 +0530

    changed the url for snipcart

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -15,7 +15,7 @@ export default function Product({ product }) {
             data-item-name={product.title}
             data-item-price={product.price}
             data-item-image={product.image.fluid.src}
-            data-item-url="https://gatsby-project-coder646.netlify.com/"
+            data-item-url="https://saluja-coder-646.netlify.com/"
           >
             add to cart
           </button>
